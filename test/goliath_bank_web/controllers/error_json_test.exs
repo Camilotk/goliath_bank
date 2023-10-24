561ce59a6d8774b7f6a2be7778ebb14534f041bb
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-10-24 11:38:54 -0300

    update error tests

--- test/goliath_bank_web/controllers/error_json_test.exs
+++ test/goliath_bank_web/controllers/error_json_test.exs
@@ -1,12 +1,15 @@
 defmodule GoliathBankWeb.ErrorJSONTest do
   use GoliathBankWeb.ConnCase, async: true
 
+  alias GoliathBank.Users.User
+
   test "renders 404" do
-    assert GoliathBankWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert GoliathBankWeb.ErrorJSON.error(%{status: :not_found}) == %{message: "User not found in database!", status: :not_found}
   end
 
   test "renders 500" do
-    assert GoliathBankWeb.ErrorJSON.render("500.json", %{}) ==
-             %{errors: %{detail: "Internal Server Error"}}
+    changeset = User.changeset(%{cpf: "1234"})
+    assert GoliathBankWeb.ErrorJSON.error(%{changeset: changeset}) ==
+      %{errors: %{cpf: ["should be 11 character(s)"], first_name: ["can't be blank"], last_name: ["can't be blank"], password: ["can't be blank"]}}
   end
 end
