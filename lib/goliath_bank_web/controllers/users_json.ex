3d2fe2d8fe9981a87a9ff46366b9f51356cc8678
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-10-24 11:10:50 -0300

    add user update

--- lib/goliath_bank_web/controllers/users_json.ex
+++ lib/goliath_bank_web/controllers/users_json.ex
@@ -1,4 +1,5 @@
 defmodule GoliathBankWeb.UsersJSON do
   def create(%{user: user}), do: %{message: "Created with success!", data: user}
   def get(%{user: user}), do: %{data: user}
+  def update(%{user: user}), do: %{data: user}
 end
