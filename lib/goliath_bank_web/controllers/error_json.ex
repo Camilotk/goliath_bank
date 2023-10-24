07f5b20aa143fef25fd47531102759e0f97c4707
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-10-24 10:24:32 -0300

    update error handling

--- lib/goliath_bank_web/controllers/error_json.ex
+++ lib/goliath_bank_web/controllers/error_json.ex
@@ -9,7 +9,11 @@ defmodule GoliathBankWeb.ErrorJSON do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
   # "Not Found".
-  def render(template, _assigns) do
-    %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+  def error(%{changeset: changeset}) do
+    %{errors: Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+      Regex.replace(~r"%{(\w+)}", msg, fn _, key ->
+        opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
+      end)
+    end)}
   end
 end
