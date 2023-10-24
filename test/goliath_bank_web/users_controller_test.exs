d33a9bde1ce6793f28690d0ef5dea890d95bf3eb
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-10-24 13:29:39 -0300

    add test for invalid parameters on create

--- test/goliath_bank_web/users_controller_test.exs
+++ test/goliath_bank_web/users_controller_test.exs
@@ -1,5 +1,6 @@
 defmodule GoliathBankWeb.UsersControllerTest do
   use GoliathBankWeb.ConnCase
+
   describe "create/2" do
     test "successfully creates an user", %{conn: conn} do
       params = %{
@@ -20,6 +21,22 @@ defmodule GoliathBankWeb.UsersControllerTest do
                             "last_name" => "de Tal",
                             "cpf" => "23456789001"
                           }, "message" => "Created with success!"} = response
+    end
+
+    test "got invalid params in user creation", %{conn: conn} do
+      params = %{
+        first_name: "Fulano",
+        last_name: "de Tal",
+        cpf: "2345678901",
+        password: "qwerty"
+      }
+
+      response =
+        conn
+        |> post(~p"/api/users", params)
+        |> json_response(:bad_request)
+
+        assert %{"errors" => %{"cpf" => ["should be 11 character(s)"]}} = response
     end
   end
 end
