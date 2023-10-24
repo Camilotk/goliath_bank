e2411a47afed9d82dd491c6049f417246cd960f0
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-10-24 11:04:39 -0300

    update error handling

--- lib/goliath_bank_web/controllers/fallback_controller.ex
+++ lib/goliath_bank_web/controllers/fallback_controller.ex
@@ -3,6 +3,13 @@ defmodule GoliathBankWeb.FallbackController do
 
   alias GoliathBankWeb.ErrorJSON
 
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(json: ErrorJSON)
+    |> render(:error, status: :not_found)
+  end
+
   def call(conn, {:error, changeset}) do
     conn
     |> put_status(:bad_request)
