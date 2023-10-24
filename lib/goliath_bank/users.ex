6673910880dd3e3d2c4351f8d4d08eb4b34f8874
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-10-24 11:03:59 -0300

    add user delegate

--- lib/goliath_bank/users.ex
+++ lib/goliath_bank/users.ex
@@ -1,5 +1,6 @@
 defmodule GoliathBank.Users do
-  alias GoliathBank.Users.Create
+  alias GoliathBank.Users.{Create, Get}
 
   defdelegate create(params), to: Create, as: :call
+  defdelegate get(id), to: Get, as: :call
 end
