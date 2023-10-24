8b91a5ed0b45c7faf2466342ccb494a8286a67ae
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-10-24 04:32:05 -0300

    update user migration

--- priv/repo/migrations/20231024070734_add_users_table.exs
+++ priv/repo/migrations/20231024070734_add_users_table.exs
@@ -3,9 +3,9 @@ defmodule GoliathBank.Repo.Migrations.AddUsersTable do
 
   def change do
     create table("users") do
-      add :name, :string, null: false
+      add :first_name, :string, null: false
+      add :last_name, :string, null: false
       add :password_hash, :string, null: false
-      add :email, :string, null: false
       add :cpf, :string, null: false
 
       timestamps()
