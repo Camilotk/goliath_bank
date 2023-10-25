7efb5a77d1a05218255eeb508df930d88fcac10c
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-10-25 13:03:22 -0300

    treat the error when a non existend user id is passed in account creation

--- lib/goliath_bank/accounts/create.ex
+++ lib/goliath_bank/accounts/create.ex
@@ -1,8 +1,16 @@
 defmodule GoliathBank.Accounts.Create do
   alias GoliathBank.Accounts.Account
+  alias GoliathBank.Users.User
   alias GoliathBank.Repo
 
-  def call(params) do
+  def call(%{"user_id" => id} = params) do
+    case Repo.get(User, id) do
+      nil -> {:error, :not_found}
+      _ -> create(params)
+    end
+  end
+
+  defp create(params) do
     params
     |> Account.changeset()
     |> Repo.insert()
