15b1a749f4f650c16049f573783a8be46dff7125
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-10-25 03:40:53 -0300

    add account controller

---
+++ lib/goliath_bank_web/controllers/accounts_controller.ex
@@ -0,0 +1,16 @@
+defmodule GoliathBankWeb.AccountsController do
+  use GoliathBankWeb, :controller
+
+  alias GoliathBank.Accounts
+  alias Accounts.Account
+
+  action_fallback GoliathBankWeb.FallbackController
+
+  def create(conn, params) do
+    with {:ok, %Account{} = account} <- Accounts.create(params) do
+      conn
+      |> put_status(:created)
+      |> render(:create, account: account)
+    end
+  end
+end
