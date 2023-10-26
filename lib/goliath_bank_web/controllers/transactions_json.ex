f7392d1d8cec2fe7a97843bef8d06c9ed2352cbe
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-10-26 17:28:26 -0300

    change the transation seach json rendering for better visualization of entries

--- lib/goliath_bank_web/controllers/transactions_json.ex
+++ lib/goliath_bank_web/controllers/transactions_json.ex
@@ -7,5 +7,15 @@ defmodule GoliathBankWeb.TransactionsJSON do
     }
   end
 
-  def search(%{transactions: transactions}), do: transactions
+  def search(%{transactions: transactions}), do: Enum.map(transactions, &render_transaction/1)
+
+  def render_transaction(transaction) do
+    %{
+      transaction_id: transaction.id,
+      sender: transaction.sender_id,
+      receiver: transaction.receiver_id,
+      amount: transaction.amount,
+      datetime: transaction.inserted_at
+    }
+  end
 end
