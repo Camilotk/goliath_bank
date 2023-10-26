88c174c951b48377d774072eac5ab1f5aefa274e
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-10-26 00:35:03 -0300

    fix pattern matching in transaction

--- lib/goliath_bank_web/controllers/transactions_controller.ex
+++ lib/goliath_bank_web/controllers/transactions_controller.ex
@@ -6,15 +6,10 @@ defmodule GoliathBankWeb.TransactionsController do
   action_fallback GoliathBankWeb.FallbackController
 
   def create(conn, %{"sender_id" => sender_id, "receiver_id" => receiver_id, "amount" => amount}) do
-    case Transactions.create(sender_id, receiver_id, Decimal.new(amount)) do
-      {:ok, %{save_in_database: transaction}} ->
+    with {:ok, %{save_in_database: transaction}} <- Transactions.create(sender_id, receiver_id, Decimal.new(amount)) do
         conn
         |> put_status(:created)
         |> render(:create, transaction: transaction)
-      # {:error, _reason} ->
-      #   conn
-      #   |> put_status(:unprocessable_entity)
-      #   |> render(GoliathBankWeb.FallbackController, "error.json")
     end
   end
 end
