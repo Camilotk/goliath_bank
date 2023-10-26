2381d6a68b387beb3041b4919d8350b2b8f07c95
Camilo Cunha de Azevedo <camilotk@gmail.com>
2023-10-26 14:52:37 -0300

    add facade to get account

--- lib/goliath_bank/accounts.ex
+++ lib/goliath_bank/accounts.ex
@@ -1,5 +1,6 @@
 defmodule GoliathBank.Accounts do
-  alias GoliathBank.Accounts.Create
+  alias GoliathBank.Accounts.{Create, Get}
 
   defdelegate create(params), to: Create, as: :call
+  defdelegate get(id), to: Get, as: :call
 end
